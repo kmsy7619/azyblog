ea15f2895ebc749fe1eee049294e5437e89e5b41
kmsy7619 <kmsy7619@users.noreply.github.com>
2026-09-10 06:42:44 +0000

    deploy: 10ca4e65ca369cf853cc7de4f6bb5781c5e3e609

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/09/09/hello-world/"];function toRandomPost(){
+var posts=["2026/09/08/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
